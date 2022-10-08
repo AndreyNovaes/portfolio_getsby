221a9cb7bd4f1eb4b863d210d0e8ad7bfe07fab1
AndreyNovaes <testederealismo@gmail.com>
2022-10-07 21:38:06 -0300

    feat: add SEO metadata config, with react helmet and plugin manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,8 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Andrey Novaes`,
+    description: `Andrey De Novaes Ferreira portfolio, personal website`,
   },
   graphqlTypegen: true,
   plugins: [
@@ -15,12 +16,29 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
-        // Setting a color is optional.
-        color: `#718096`,
+        color: `#718096`, // color of the progress bar
         minimum: 0.8,
         showSpinner: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-react-helmet`,
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Andrey Novaes`,
+        short_name: `Andrey Novaes`,
+        start_url: `/`,
+        icon: `./src/assets/images/portfolio.png`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-1asdfa75000-1fdasfas",
+    //   },
+    // },
   ],
 }
 
