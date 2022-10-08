efeaa1572be37a2ae4fed4aed29b57f75bf78cb7
AndreyNovaes <testederealismo@gmail.com>
2022-10-07 21:38:41 -0300

    feat: add SEO hardcoded in Layout component

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import './src/styles/global.css'
 
+
 // Nprogress config
 import NProgress, { NProgressOptions } from "nprogress"
 
@@ -34,16 +35,35 @@ export const onRouteUpdate = ({ location, prevLocation }) => {
 // Layout config with gatsby wrapper
 import Layout from './src/Layout'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { Helmet } from "react-helmet"
 
 export const wrapPageElement = ({ element, props }) => {
   // Wraps every page in a component
-  theme.config.initialColorMode = "dark"
+  theme.fonts.heading = 'Poppins, sans-serif'
+  theme.fonts.body = 'Poppins, sans-serif'
+  theme.config.initialColorMode = 'dark'
+
   return (
     <>
       <ChakraProvider theme={theme}>
-        <Layout {...props}>{element}</Layout>
+        <Layout {...props}>
+          <Helmet>
+            <title>Andrey Novaes</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Andrey Novaes portfolio" />
+            <meta name="keywords" content="Andrey Novaes, Andrey Novaes portfolio, Andrey Novaes site, Andrey De Novaes Ferreira portfolio, Andrey personal website, Andrey De Novaes Ferreira personal website, Andrey De Novaes Ferreira" />
+            <meta name="author" content="Andrey Novaes" />
+            <meta property="og:title" content="Andrey Novaes portfólio" />
+            <meta property="og:description" content="portfólio de Andrey Novaes" />
+            <meta property="og:url" content="https://andrey-novaes.me" />
+            <meta property="og:site_name" content="Andrey Novaes portfólio" />
+            <meta property="og:type" content="website" />
+            <meta property="og:locale" content="pt-BR" />
+            <link rel="canonical" href="https://andrey-novaes.me" />
+          </Helmet>
+          {element}
+        </Layout>
       </ChakraProvider>
     </>
   )
 };
-// Layout config with gatsby wrapper
