ca641c20657289ea68b0a67ea3094253781e3fd3
AndreyNovaes <testederealismo@gmail.com>
2022-10-07 22:56:50 -0300

    chore: moving about section to components

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import {
   Box,
-  Flex,
-  HStack,
-  IconButton,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-  Link,
-  Button,
-  Center,
 } from '@chakra-ui/react';
 
 export default function Projects() {
