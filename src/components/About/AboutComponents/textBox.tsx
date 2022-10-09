5faaaf9403f5b8f615bc10cf24f0be14d319b765
AndreyNovaes <testederealismo@gmail.com>
2022-10-08 22:26:29 -0300

    fix: fix text align in mobile

--- src/components/About/AboutComponents/textBox.tsx
+++ src/components/About/AboutComponents/textBox.tsx
@@ -17,12 +17,12 @@ export default function TextBox() {
     <>
       <Stack spacing="4" maxW={{ md: '2xl' }}>
         <Heading 
-          as="h1"
           fontSize={{ base: '3xl', md: '4xl' }}
           fontWeight="extrabold"
           letterSpacing="wide"
           lineHeight="shorter"
           color={useColorModeValue('gray.900', 'white')}
+          textAlign="center"
         >
           Como conheci a programação
         </Heading>
@@ -33,27 +33,29 @@ export default function TextBox() {
           letterSpacing="wide"
           lineHeight="tall"
         >
-          <Box>
-            <Box
-              fontSize={{ base: 'md', md: 'lg' }}
-              color={useColorModeValue('gray.500', 'gray.400')}
-            >
-              <Box as="p" color={useColorModeValue('brand.600', 'brand.400')}>
-                Ex jogador profissional de e-sports, me vi desencantado e decidi buscar uma nova carreira, encontrei o&nbsp;
-                <Link color='telegram.400' as={gatsbyLink} target='_blank' to='https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&ab_channel=CursoemV%C3%ADdeo'  >
-                  curso de python do Gustavo Guanabara
-                </Link>
-                &nbsp;no youtube, e, a partir daí que eu tive certeza que
-                era isso que eu queria fazer da minha vida.
-              </Box>
-              <Box as="p" color={useColorModeValue('brand.600', 'brand.400')}>
+          <Box
+            fontSize={{ base: 'md', md: 'lg' }}
+            color={useColorModeValue('gray.500', 'gray.400')}
+          >
+            <Text color={useColorModeValue('brand.600', 'brand.400')} align={{ base: 'center', md: 'left' }}>
+              Ex jogador profissional de e-sports, me vi desencantado e decidi buscar uma nova carreira, encontrei o&nbsp;
+              <Link color='telegram.400' as={gatsbyLink} target='_blank' to='https://www.youtube.com/watch?v=S9uPNppGsGo&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&ab_channel=CursoemV%C3%ADdeo'>
+                curso de python do Gustavo Guanabara
+              </Link>
+              &nbsp;no youtube, e, a partir daí que eu tive certeza que
+              era isso que eu queria fazer da minha vida.
+            </Text>
+              <br />
+              <Text color={useColorModeValue('brand.600', 'brand.400')} align={{ base: 'center', md: 'left' }}> 
                 Entrei em uma 
                 faculdade de sistemas de informação, onde tive meu primeiro contato com a programação web, porém, não
-                foi o suficiente para me satisfazer, era um curso lento, desatualizado e estático, queria algo mais dinâmico e rápido
-                então comecei a pesquisar sobre outros cursos e ferramentas para estudar por conta própria, acabei encontrando o curso
-                de desenvolvimento web da Trybe, onde aprendi a programar em javascript, react, node, entre outras tecnologias.
-              </Box>
-              <Box as="p" color={useColorModeValue('brand.600', 'brand.400')}>
+                sentia que gerava valor em relação a programação, era um curso lento, desatualizado e estático, queria algo mais 
+                dinâmico e rápido então comecei a pesquisar sobre outros cursos e ferramentas para estudar por conta própria,
+                acabei encontrando o curso de desenvolvimento web da Trybe, onde aprendi a programar em javascript, react, node,
+                entre outras tecnologias.
+              </Text>
+              <br />
+              <Text as="p" color={useColorModeValue('brand.600', 'brand.400')} align={{ base: 'center', md: 'left' }}>
                 Me espelho em grandes mestres como&nbsp;
                 <Link color='telegram.400' as={gatsbyLink} target='_blank' to='https://www.youtube.com/c/RodrigoBranas'  >
                   Rodrigo Branas
@@ -63,7 +65,7 @@ export default function TextBox() {
                   Rodrinho Manguinho
                 </Link>
                 &nbsp;,&nbsp;
-                <Link color='telegram.400' as={gatsbyLink} target='_blank' to='https://www.youtube.com/c/FullCycle'  >
+                <Link color='telegram.400' as={gatsbyLink} target='_blank' to='https://www.youtube.com/c/FullCycle' >
                   Wesley Willians
                 </Link>
                 &nbsp;e&nbsp;
@@ -71,16 +73,14 @@ export default function TextBox() {
                   Otavio Lemos
                 </Link>
                 &nbsp;e espero um dia chegar ao nível deles.
-              </Box>
-              <Box as="p" color={useColorModeValue('brand.600', 'brand.400')}>
+              </Text>
+              <br />
+              <Text as="p" color={useColorModeValue('brand.600', 'brand.400')} align={{ base: 'center', md: 'left' }}>
                 Hoje, já formado pela Trybe, busco novas oportunidades para continuar aprendendo, pretendo, quando tiver uma 
                 maior senioridade, ajudar pessoas que estão começando agora como eu, tentando sempre tornar algo que é difícil e 
                 complicado, simples e fácil.
-              </Box>
-              {/* <Box as="p" color={useColorModeValue('brand.600', 'brand.400')}> */}
-                
+              </Text>
             </Box>
-          </Box>
         </Box>
       </Stack>
     </>
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -20,6 +20,7 @@ export default function About() {
         <Center>
           <TextBox />
         </Center>
+        <br />
         <ImageBox />
       </Stack>
     </>
